8526c302f05add56ce16b8977c75f6f639275b9d
Vinny Pavoni <vinpavoni@gmail.com>
2025-06-27 15:09:40 +0200

    Add Google Ads meta tag and script, test head title

--- app/head.tsx
+++ app/head.tsx
@@ -1,15 +1,13 @@
 export default function Head() {
   return (
     <>
-      <meta
-        name="google-adsense-account"
-        content="ca-pub-8274422118651919"
-      />
+      <meta name="google-adsense-account" content="ca-pub-8274422118651919" />
       <script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
         data-ad-client="ca-pub-8274422118651919"
       />
+      <title>TEST HEAD - Touchdown Bingo</title>
     </>
   );
 }
